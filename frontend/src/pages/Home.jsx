be15a368585007ff340a71c4297770b95518b845
Ashwani Kumar Singh <163728607+ashwani2006@users.noreply.github.com>
2025-06-01 11:04:44 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -75,6 +75,7 @@ const Home = () => {
   return (
     <div>
        <div>
+         <h1>prite ka bhosda<h1/>
          <h1>Hello... { username === ''? (<span>Guest</span>):(<span>{username}💝</span>)}</h1>
        </div>
         <hr />
